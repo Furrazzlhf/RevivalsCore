1ea183f2da152e1b4caf79e69ebf8d67a20be459
Morgar <lecarp@hotmail.fr>
2017-11-10 11:11:05 +0100

    RevivalsCore
    RevivalsCore

---
+++ src/server/game/Handlers/BattlefieldHandler.cpp
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "WorldSession.h"
+#include "Battlefield.h"
+#include "BattlefieldMgr.h"
+#include "BattlefieldPackets.h"
+#include "Log.h"
+#include "Opcodes.h"
+#include "Player.h"
+#include "WorldPacket.h"
+
+/**
+ * @fn void WorldSession::SendBattlefieldInvitePlayerToWar(uint32 battleId, uint32 zoneId, uint32 acceptTime)
+ *
+ * @brief Sends join war frame to the player
+ *
+ * @param battleId   BattlefieldId enum
+ * @param zoneId     Zone where the battlefield is
+ * @param acceptTime Time in seconds displayed
+ */
+void WorldSession::SendBattlefieldInvitePlayerToWar(uint32 battleId, uint32 zoneId, uint32 acceptTime)
+{
+    WorldPackets::Battlefield::BattlefieldMgrEntryInvite entryInvite;
+    entryInvite.BattleID = battleId;
+    entryInvite.ZoneID = zoneId;
+    entryInvite.ExpireTime = uint32(time(nullptr) + acceptTime);
+    SendPacket(entryInvite.Write());
+}
+
+/**
+ * @fn void WorldSession::SendBattlefieldInvitePlayerToQueue(uint32 battleId)
+ *
+ * @brief Sends join queue frame to the player
+ *
+ * @param battleId BattlefieldId enum
+ */
+void WorldSession::SendBattlefieldInvitePlayerToQueue(uint32 battleId)
+{
+    WorldPackets::Battlefield::BattlefieldMgrQueueInvite queueInvite;
+    queueInvite.BattleID = battleId;
+    queueInvite.Warmup = 1; // not implemented
+    SendPacket(queueInvite.Write());
+}
+
+/**
+ * @fn void WorldSession::SendBattlefieldQueueInviteResponse(uint32 battleId, uint32 zoneId, bool canQueue, bool full)
+ *
+ * @brief Sends queue information to the player
+ *
+ * @param battleId BattlefieldId enum
+ * @param zoneId   Zone where the battlefield is
+ * @param canQueue Inform if the player can join queue or not
+ * @param full     Inform if its full or not
+ */
+void WorldSession::SendBattlefieldQueueInviteResponse(uint32 battleId, uint32 zoneId, bool canQueue, bool full)
+{
+    WorldPackets::Battlefield::BattlefieldMgrQueueRequestResponse queueRequestResponse;
+    queueRequestResponse.BattleID = battleId;
+    queueRequestResponse.ZoneID = zoneId;
+    queueRequestResponse.Accepted = canQueue ? 1 : 0;
+    queueRequestResponse.LoggingIn = !full;
+    queueRequestResponse.Warmup = 1; // not implemented
+    SendPacket(queueRequestResponse.Write());
+}
+
+/**
+ * @fn void WorldSession::SendBattlefieldEntered(uint32 battleId)
+ *
+ * @brief Sent when the player joins a battlefield
+ *
+ * @param battleId BattlefieldId enum
+ */
+void WorldSession::SendBattlefieldEntered(uint32 battleId)
+{
+    WorldPackets::Battlefield::BattlefieldMgrEntered entered;
+    entered.BattleID = battleId;
+    entered.OnOffense = true; // not implemented
+    entered.Relocated = true; // not implemented
+    entered.ClearedAFK = _player->isAFK();
+    SendPacket(entered.Write());
+}
+
+/**
+ * @fn void WorldSession::SendBattlefieldLeaveMessage(uint32 battleId, BFLeaveReason reason)
+ *
+ * @brief Sent when the player leaves the battlefield
+ *
+ * @param battleId BattlefieldId enum
+ * @param reason   Reason why player the left
+ */
+void WorldSession::SendBattlefieldLeaveMessage(uint32 battleId, BFLeaveReason reason /*= BF_LEAVE_REASON_EXITED*/)
+{
+    WorldPackets::Battlefield::BattlefieldMgrEjected ejected;
+    ejected.BattleID = battleId;
+    ejected.Reason = uint8(reason);
+    ejected.BattleStatus = 2; // not implemented
+    ejected.Relocated = false; // not implemented
+    SendPacket(ejected.Write());
+}
+
+/**
+* @fn void WorldSession::SendBattlefieldEjectPending(bool remove)
+*
+* @brief Sent when the player is about to be kicked from the battlefield
+*
+* @param battleId BattlefieldId enum
+* @param remove   Signals if the player is going to be relocated
+*/
+void WorldSession::SendBattlefieldEjectPending(uint32 battleId, bool remove)
+{
+    WorldPackets::Battlefield::BattlefieldMgrEjectPending ejectPending;
+    ejectPending.BattleID = battleId;
+    ejectPending.Remove = !remove;
+    SendPacket(ejectPending.Write());
+}
+
+/**
+* @fn void WorldSession::HandleBattlefieldEntryInviteResponse(WorldPackets::Battlefield::BattlefieldMgrEntryInviteResponse& entryInviteResponse)
+*
+* @brief Sent on join to battlefield war response
+*/
+void WorldSession::HandleBattlefieldEntryInviteResponse(WorldPackets::Battlefield::BattlefieldMgrEntryInviteResponse& entryInviteResponse)
+{
+    uint32 battleId = entryInviteResponse.BattleID;
+    bool accepted = entryInviteResponse.AcceptedInvite;
+    TC_LOG_DEBUG("misc", "WorldSession::HandleBattlefieldEntryInviteResponse: battleId: %u, accepted: %u", battleId, accepted);
+
+    Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(BattlefieldId(battleId));
+    if (!battlefield)
+        return;
+
+    if (accepted)
+        battlefield->PlayerAcceptsInviteToWar(_player);
+    else
+        battlefield->PlayerLeavesQueue(_player, true);
+}
+
+/**
+ * @fn void WorldSession::HandleBattlefieldQueueInviteResponse(WorldPackets::Battlefield::BattlefieldMgrQueueInviteResponse& queueInviteResponse)
+ *
+ * @brief Sent on battlefield queue response
+ */
+void WorldSession::HandleBattlefieldQueueInviteResponse(WorldPackets::Battlefield::BattlefieldMgrQueueInviteResponse& queueInviteResponse)
+{
+    uint32 battleId = queueInviteResponse.BattleID;
+    bool accepted = queueInviteResponse.AcceptedInvite;
+    TC_LOG_DEBUG("misc", "WorldSession::HandleBattlefieldQueueInviteResponse: battleId: %u, accepted: %u", battleId, accepted);
+
+    Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(BattlefieldId(battleId));
+    if (!battlefield)
+        return;
+
+    if (accepted)
+        battlefield->PlayerAcceptsInviteToQueue(_player);
+}
+
+/**
+ * @fn void WorldSession::HandleBattlefieldExitRequest(WorldPackets::Battlefield::BattlefieldMgrExitRequest& exitRequest)
+ *
+ * @brief Sent on battlefield queue exit request
+ */
+void WorldSession::HandleBattlefieldExitRequest(WorldPackets::Battlefield::BattlefieldMgrExitRequest& exitRequest)
+{
+    uint32 battleId = exitRequest.BattleID;
+    TC_LOG_DEBUG("misc", "HandleBfExitRequest: battleId: %u ", battleId);
+
+    Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(BattlefieldId(battleId));
+    if (!battlefield)
+        return;
+
+    battlefield->PlayerLeavesQueue(_player);
+}
---
+++ src/server/scripts/Commands/cs_bf.cpp
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ScriptData
+Name: bf_commandscript
+%Complete: 100
+Comment: All bf related commands
+Category: commandscripts
+EndScriptData */
+
+#include "ScriptMgr.h"
+#include "Battlefield.h"
+#include "BattlefieldMgr.h"
+#include "Chat.h"
+#include "Language.h"
+#include "RBAC.h"
+
+class bf_commandscript : public CommandScript
+{
+public:
+    bf_commandscript() : CommandScript("bf_commandscript") { }
+
+    static std::string GetBattleIdString(BattlefieldId battleId)
+    {
+        switch (battleId)
+        {
+            case BATTLEFIELD_BATTLEID_WINTERGRASP:
+                return "Wintergrasp";
+            default:
+                return "";
+        }
+    }
+
+    std::vector<ChatCommand> GetCommands() const override
+    {
+        static std::vector<ChatCommand> battlefieldcommandTable =
+        {
+            { "start",          rbac::RBAC_PERM_COMMAND_BF_START,  false, &HandleBattlefieldStart,  "" },
+            { "stop",           rbac::RBAC_PERM_COMMAND_BF_STOP,   false, &HandleBattlefieldEnd,    "" },
+            { "switch",         rbac::RBAC_PERM_COMMAND_BF_SWITCH, false, &HandleBattlefieldSwitch, "" },
+            { "timer",          rbac::RBAC_PERM_COMMAND_BF_TIMER,  false, &HandleBattlefieldTimer,  "" },
+            { "enable",         rbac::RBAC_PERM_COMMAND_BF_ENABLE, false, &HandleBattlefieldEnable, "" },
+        };
+        static std::vector<ChatCommand> commandTable =
+        {
+            { "bf",             rbac::RBAC_PERM_COMMAND_BF,        false, nullptr,                  "", battlefieldcommandTable },
+        };
+        return commandTable;
+    }
+
+    static bool HandleBattlefieldStart(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* battleidstr = strtok((char*)args, " ");
+        if (!battleidstr)
+            return false;
+
+        uint32 battlearg = atoul(battleidstr);
+        if (battlearg == 0 || battlearg >= BATTLEFIELD_BATTLEID_MAX)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_INCORRECT_ID);
+            return true;
+        }
+
+        BattlefieldId battleId = BattlefieldId(battlearg);
+        Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(battleId);
+        if (!battlefield)
+            return true;
+
+        if (!battlefield->IsEnabled())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_DISABLED, GetBattleIdString(battleId));
+            return true;
+        }
+
+        if (battlefield->IsWarTime())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_ACTIVE, GetBattleIdString(battleId));
+            return true;
+        }
+
+        battlefield->StartBattle();
+        handler->PSendSysMessage(LANG_COMMAND_BF_START, GetBattleIdString(battleId));
+        return true;
+    }
+
+    static bool HandleBattlefieldEnd(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* battleidstr = strtok((char*)args, " ");
+        if (!battleidstr)
+            return false;
+
+        uint32 battlearg = atoul(battleidstr);
+        if (battlearg == 0 || battlearg >= BATTLEFIELD_BATTLEID_MAX)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_INCORRECT_ID);
+            return true;
+        }
+
+        BattlefieldId battleId = BattlefieldId(battlearg);
+        Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(battleId);
+        if (!battlefield)
+            return true;
+
+        if (!battlefield->IsEnabled())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_DISABLED, GetBattleIdString(battleId));
+            return true;
+        }
+
+        if (!battlefield->IsWarTime())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_ALREADY_STOPPED, GetBattleIdString(battleId));
+            return true;
+        }
+
+        battlefield->EndBattle(true);
+        handler->PSendSysMessage(LANG_COMMAND_BF_STOP, GetBattleIdString(battleId));
+        return true;
+    }
+
+    static bool HandleBattlefieldEnable(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* battleidstr = strtok((char*)args, " ");
+        if (!battleidstr)
+            return false;
+
+        uint32 battlearg = atoul(battleidstr);
+        if (battlearg == 0 || battlearg >= BATTLEFIELD_BATTLEID_MAX)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_INCORRECT_ID);
+            return true;
+        }
+
+        BattlefieldId battleId = BattlefieldId(battlearg);
+        Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(battleId);
+        if (!battlefield)
+            return true;
+
+        if (battlefield->IsEnabled())
+        {
+            battlefield->ToggleBattlefield(false);
+            handler->PSendSysMessage(LANG_COMMAND_BF_DISABLE, GetBattleIdString(battleId));
+        }
+        else
+        {
+            battlefield->ToggleBattlefield(true);
+            handler->PSendSysMessage(LANG_COMMAND_BF_ENABLE, GetBattleIdString(battleId));
+        }
+        return true;
+    }
+
+    static bool HandleBattlefieldSwitch(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* battleidstr = strtok((char*)args, " ");
+        if (!battleidstr)
+            return false;
+
+        uint32 battlearg = atoul(battleidstr);
+        if (battlearg == 0 || battlearg >= BATTLEFIELD_BATTLEID_MAX)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_INCORRECT_ID);
+            return true;
+        }
+
+        BattlefieldId battleId = BattlefieldId(battlearg);
+        Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(battleId);
+        if (!battlefield)
+            return true;
+
+        if (battlefield->IsWarTime())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_ACTIVE, GetBattleIdString(battleId));
+            return true;
+        }
+
+        battlefield->EndBattle(false);
+        handler->PSendSysMessage(LANG_COMMAND_BF_SWITCH, GetBattleIdString(battleId));
+        return true;
+    }
+
+    static bool HandleBattlefieldTimer(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* battleidstr = strtok((char*)args, " ");
+        if (!battleidstr)
+            return false;
+
+        char* timestr = strtok(nullptr, " ");
+        if (!timestr)
+            return false;
+
+        uint32 battlearg = atoul(battleidstr);
+        if (battlearg == 0 || battlearg >= BATTLEFIELD_BATTLEID_MAX)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_INCORRECT_ID);
+            return true;
+        }
+
+        BattlefieldId battleId = BattlefieldId(battlearg);
+        Battlefield* battlefield = sBattlefieldMgr->GetBattlefield(battleId);
+        if (!battlefield)
+            return true;
+
+        if (!battlefield->IsEnabled())
+        {
+            handler->PSendSysMessage(LANG_COMMAND_BF_DISABLED, GetBattleIdString(battleId));
+            return true;
+        }
+
+        uint32 time = atoul(timestr);
+        battlefield->SetTimer(time * IN_MILLISECONDS);
+        battlefield->SendInitWorldStatesToAll();
+        handler->PSendSysMessage(LANG_COMMAND_BF_TIMER, GetBattleIdString(battleId), battlefield->GetTimer(), battlefield->IsWarTime() ? "war time" : "no war time");
+        return true;
+    }
+};
+
+void AddSC_bf_commandscript()
+{
+    new bf_commandscript();
+}
